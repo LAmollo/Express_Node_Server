1653cf1c1ce7f10e5e772bb2cdd1c8476fe5e43c
LAmollo <lawinyamuga@gmail.com>
2024-06-16 11:05:34 -0700

    routes

--- app.js
+++ app.js
@@ -1,50 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
+const { users, posts, comments } = require('./data');
 
 // Middleware
-const requestLogger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  };
-  
-  const checkApiAccess = (req, res, next) => {
-    if (req.url.includes('/api')) {
-      console.log('API Access');
-    }
-    next();
-  };
-  
-  const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  };
-  
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  app.use(requestLogger);
-  app.use(checkApiAccess);
-  app.use(express.static('public'));
+const setTheme = (req, res, next) => {
+  if (req.query.theme) {
+    res.locals.theme = req.query.theme;
+  } else {
+    res.locals.theme = req.cookies.theme || 'light';
+  }
+  next();
+};
 
-// Set EJS as the template engine
-  app.set('view engine', 'ejs');
+const requestLogger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+};
+
+// Middleware setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use(setTheme);
+app.use(requestLogger);
+
+// View engine setup
+app.set('view engine', 'ejs');
 
 // Routes
-  const indexRouter = require('./routes/index');
-  const usersRouter = require('./routes/users');
-  const postsRouter = require('./routes/posts');
-  const commentsRouter = require('./routes/comments');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const commentsRouter = require('./routes/comments');
 
-  app.use('/', indexRouter);
-  app.use('/api/users', usersRouter);
-  app.use('/api/posts', postsRouter);
-  app.use('/api/comments', commentsRouter);
+app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/posts', postsRouter);
+app.use('/api/comments', commentsRouter);
 
 // Error handling middleware
-  app.use(errorHandler);
+app.use(errorHandler);
 
-  app.listen(port, () => {
+// Start server
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
- });
-
+});
