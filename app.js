e557dbca609b824af6497cb99465b258bf299794
LAmollo <lawinyamuga@gmail.com>
2024-06-16 10:04:26 -0700

    middleware

--- app.js
+++ app.js
@@ -2,3 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+// Middleware
+const requestLogger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  };
+  
+  const checkApiAccess = (req, res, next) => {
+    if (req.url.includes('/api')) {
+      console.log('API Access');
+    }
+    next();
+  };
+  
+  const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  };
+  
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(requestLogger);
+  app.use(checkApiAccess);
+  app.use(express.static('public'));
+
